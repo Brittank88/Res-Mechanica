f1bab51962c89021c53307ddefb636de1fbcf1c9
Mason Hunt <masonhunt130ii@gmail.com>
2021-01-17 02:50:36 +1100

    Many Changes
    - WE Wand finished; thanks Darkpuppey for the item texture! <3
    - Fixed Blood Magic hellforged items missing lang.
    - Hid some dev / clutter items from JEI.
    - Added PneumaticCraft Repressurised
    - Added ItemZoom Rekindled
    - Added RFTools Storage

--- kubejs/client_scripts/script.js
+++ kubejs/client_scripts/script.js
@@ -2,7 +2,18 @@
 
 console.info('Hello, World! (You will see this line every time client resources reload)')
 
+/// ### JEI ITEM HIDING ### ///
+
 onEvent('jei.hide.items', event => {
-  // Hide items in JEI here
-  // event.hide('minecraft:cobblestone')
+  // Hide items in JEI here.
+  for (let item of [
+    'appliedenergistics2:facade',
+    'woot:debug',
+    'woot:debug_tank',
+    'zerocore:debugtool',
+    'cyclic:item_infinite'
+  ]) {
+    event.hide(item)
+    console.info(`Removed item '${item}' from JEI`)
+  }
 })
--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -2,9 +2,18 @@
 
 console.info('Hello, World! (You will only see this line once in console, during startup)')
 
-onEvent('item.registry', event => {
-  // Register new items here
-  // event.create('example_item').displayName('Example Item')
+/// ### CUSTOM ITEMS ### ///
+
+// Listen to block registry event.
+events.listen('item.registry', function (event) {
+  // The texture for this item has to be placed in kubejs/assets/kubejs/textures/item/test_item.png.
+  // If you want a custom item model, you can create one in Blockbench and put it in kubejs/assets/kubejs/models/item/test_item.json.
+  for (let item of [
+    { 'id': 'worldedit:wand', 'display': 'WorldEdit Wand', 'tooltip': 'Credits to §eDarkpuppey§r for the texture! §d<3§r' }
+  ]) {
+    event.create(item['id']).displayName(item['display']).tooltip(item['tooltip'])
+    console.info(`Registered item '${item['display']}' with ID '${item['id']}'`)
+  }
 })
 
 onEvent('block.registry', event => {
