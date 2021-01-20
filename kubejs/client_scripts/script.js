273bfa9d46ea2b92213ea728a74b2d05359e0cb2
Brittank88 <masonhunt130ii@gmail.com>
2021-01-20 16:43:40 +1100

    Multiple Changes
    - Ping buttons in radial menu (thanks Darkpuppey <3)
    - Abusing F-key binds.
    - Forgetting the other changes I made. :x

--- kubejs/client_scripts/script.js
+++ kubejs/client_scripts/script.js
@@ -1,19 +1,28 @@
 // priority: 0
 
-console.info('Hello, World! (You will see this line every time client resources reload)')
+console.info('Hello, World! (You will see this line every time client resources reload)');
 
 /// ### JEI ITEM HIDING ### ///
 
 onEvent('jei.hide.items', event => {
-  // Hide items in JEI here.
-  for (let item of [
-    'appliedenergistics2:facade',
-    'woot:debug',
-    'woot:debug_tank',
-    'zerocore:debugtool',
-    'cyclic:item_infinite'
-  ]) {
-    event.hide(item)
-    console.info(`Removed item '${item}' from JEI`)
-  }
-})
+	// Hide items in JEI here.
+	for (let item of [
+		// AE2
+		'appliedenergistics2:facade',
+		// WOOT
+		'woot:debug',
+		'woot:debug_tank',
+		// ZeroCore
+		'zerocore:debugtool',
+		// Cyclic
+		'cyclic:item_infinite',
+		// Ping Icons (Custom)
+		'ping:icon_alert',
+		'ping:icon_goto',
+		'ping:icon_look',
+		'ping:icon_mine'
+	]) {
+		event.hide(item);
+		console.info(`Removed item '${item}' from JEI`);
+	}
+});
--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -1,22 +1,46 @@
 // priority: 0
 
-console.info('Hello, World! (You will only see this line once in console, during startup)')
+console.info('Hello, World! (You will only see this line once in console, during startup)');
 
 /// ### CUSTOM ITEMS ### ///
 
 // Listen to block registry event.
-events.listen('item.registry', function (event) {
-  // The texture for this item has to be placed in kubejs/assets/kubejs/textures/item/test_item.png.
-  // If you want a custom item model, you can create one in Blockbench and put it in kubejs/assets/kubejs/models/item/test_item.json.
-  for (let item of [
-    { 'id': 'worldedit:wand', 'display': 'WorldEdit Wand', 'tooltip': 'Credits to §eDarkpuppey§r for the texture! §d<3§r' }
-  ]) {
-    event.create(item['id']).displayName(item['display']).tooltip(item['tooltip'])
-    console.info(`Registered item '${item['display']}' with ID '${item['id']}'`)
-  }
-})
+events.listen('item.registry', function(event) {
+	// The texture for this item has to be placed in kubejs/assets/kubejs/textures/item/test_item.png.
+	// If you want a custom item model, you can create one in Blockbench and put it in kubejs/assets/kubejs/models/item/test_item.json.
+	for (let item of [
+		{
+			id      : 'worldedit:wand',
+			display : 'WorldEdit Wand',
+			tooltip : 'Credits to §eDarkpuppey§r for the texture! §d<3§r'
+		},
+		{
+			id      : 'ping:icon_alert',
+			display : 'Alert',
+			tooltip : 'Credits to §eDarkpuppey§r for the texture! §d<3§r'
+		},
+		{
+			id      : 'ping:icon_goto',
+			display : 'Goto',
+			tooltip : 'Credits to §eDarkpuppey§r for the texture! §d<3§r'
+		},
+		{
+			id      : 'ping:icon_look',
+			display : 'Look',
+			tooltip : 'Credits to §eDarkpuppey§r for the texture! §d<3§r'
+		},
+		{
+			id      : 'ping:icon_mine',
+			display : 'Mine',
+			tooltip : 'Credits to §eDarkpuppey§r for the texture! §d<3§r'
+		}
+	]) {
+		event.create(item['id']).displayName(item['display']).tooltip(item['tooltip']);
+		console.info(`Registered item '${item['display']}' with ID '${item['id']}'`);
+	}
+});
 
 onEvent('block.registry', event => {
-  // Register new blocks here
-  // event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
-})
+	// Register new blocks here
+	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
+});
